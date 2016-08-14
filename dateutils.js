b572dce6d17f37fa8c2ce1888441ec203092d8d0
BHARATH CHEKURI <chekuribharathtpt@gmail.com>
2016-08-14 16:16:29 -0400

    Some new functions added
    Some new functions added

--- dateutils.js
+++ dateutils.js
@@ -11,34 +11,34 @@
 var utils = require("utils");
 
 /**
- * All the date operations are added to this exports variable
+ * <p>All the date operations are added to this exports variable</P>
  */
 var dateutils = module.exports = {};
 
 /**
- * Month names with first letter capital
+ * <p>Month names with first letter capital<p>
  */
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 /**
- * Month short names with first letter capital (only first 3 characters)
+ * <p>Month short names with first letter capital (only first 3 characters)</p>
  */
 const MONTHS_SHORT_NAME = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 /**
- * Days full names with first letter capital
+ * <p>Days full names with first letter capital</p>
  */
 const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 /**
- * Days short names with first letter capital (only first 3 characters)
+ * <p>Days short names with first letter capital (only first 3 characters)</p>
  */
 const DAYS_SHORT_NAME = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 /**
- * function is used to find object is date or not
+ * <p>function is used to find object is date or not<p>
  * 
- * @param 		Date
+ * @param 		Date - valid date 
  * 
  * @return 		true when input type is date object
  * 				false when input type is other then date object
@@ -53,9 +53,9 @@ dateutils.isDate = function(date) {
 };
 
 /**
- * function is used to find object is date or not
+ * <p>function is used to find object is not date or date<p>
  * 
- * @param 		Date
+ * @param 		Date - valid date input
  * 
  * @return 		false when input type is date object
  * 				true when input type is other then date object
@@ -70,9 +70,9 @@ dateutils.isNotDate = function(date) {
 };
 
 /**
- * gives you current local system date object
+ * <p>gives you current local system date object</p>
  * 
- * @return 		Date
+ * @return 		Date - returns valid current date
  * 
  * @throws 		no exception
  */
@@ -81,9 +81,9 @@ dateutils.now = function() {
 }
 
 /**
- * returns array of week days full name
+ * <p>returns array of week days full name</P>
  * 
- * @return		String[]
+ * @return		String[] - array of full day names
  * 
  * @throws		no exception
  */
@@ -92,9 +92,9 @@ dateutils.getDays = function(){
 }
 
 /**
- * returns array of week days short name (only first 3 characters)
+ * <p>returns array of week days short name (only first 3 characters)</p>
  * 
- * @return		String[]
+ * @return		String[] - array of day names in short form.
  * 
  * @throws		no exception
  */
@@ -103,9 +103,9 @@ dateutils.getShortDays = function(){
 }
 
 /**
- * returns array of months full name
+ * <p>returns array of months full name</p>
  * 
- * @return		String[]
+ * @return		String[] - array of full month name
  * 
  * @throws		no exception
  */
@@ -114,9 +114,9 @@ dateutils.getMonths = function(){
 }
 
 /**
- * returns array of months short name (only first 3 characters)
+ * <p>returns array of months short name (only first 3 characters)</p>
  * 
- * @return		String[]
+ * @return		String[] - array of short month names
  * 
  * @throws		no exception
  */
@@ -124,4 +124,32 @@ dateutils.getShortMonths = function(){
 	return MONTHS_SHORT_NAME;
 }
 
+
+/**
+ * <p>
+ * 		private method to get month full name
+ * 		input month number should start from 0
+ *      0 <= input month number <= 11
+ * </p>
+ * 
+ * @param	Number - month number
+ * 
+ * @return	String  - month full name
+ * 
+ * @throws Exception when we don't provide valid input month number
+ */
+function _provateGetMonthName(month) {
+	if(month < 0 || month > 11) {
+		throw new Error("The month number mush be grater then equal to 0 and lesser then equal to 11");
+	}
+	return MONTHS[month];
+}
+
+
+/**
+ * 
+ */
+dateutils.getMonthName = function(monthNumberOrDate) {
+	if(utils.is)
+}
 
